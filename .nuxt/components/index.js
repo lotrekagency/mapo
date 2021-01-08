b839eb5648638665694f177b765f644821678b65
bnznamco <gabriele.baldi.01@gmail.com>
2021-01-08 10:07:54 +0100

    removed .nuxt folder

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from '../../components/Logo.vue'
-export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
-
-export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
-export const LazyVuetifyLogo = import('../../components/VuetifyLogo.vue' /* webpackChunkName: "components/VuetifyLogo" */).then(c => c.default || c)
