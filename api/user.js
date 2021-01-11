254f8a2ac31ef696e88b9229c20ef9031195123a
bnznamco <gabriele.baldi.01@gmail.com>
2021-01-11 11:36:17 +0100

    change camomilla api defult endpoint

--- api/user.js
+++ api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: process.env.AUTH_API || '/api/api-token-auth/',
+    url: process.env.AUTH_API || '/api/camomilla/token-auth/',
     method: 'post',
     data
   })
