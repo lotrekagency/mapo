59652e0ceed9d419e6e7794dba1dee5ea301d38e
andreamorosi <andreamorosi94@gmail.com>
2021-01-12 14:00:22 +0100

    somehow .nuxt files

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,15 +1,15 @@
 export { default as Logo } from '../../components/Logo.vue'
 export { default as LogoutButton } from '../../components/LogoutButton.vue'
 export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
+export { default as Topbar } from '../../components/topbar/Topbar.vue'
 export { default as SidebarFooter } from '../../components/sidebar/SidebarFooter.vue'
 export { default as SidebarList } from '../../components/sidebar/SidebarList.vue'
 export { default as SidebarListItem } from '../../components/sidebar/SidebarListItem.vue'
-export { default as Topbar } from '../../components/topbar/Topbar.vue'
 
 export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
 export const LazyLogoutButton = import('../../components/LogoutButton.vue' /* webpackChunkName: "components/LogoutButton" */).then(c => c.default || c)
 export const LazyVuetifyLogo = import('../../components/VuetifyLogo.vue' /* webpackChunkName: "components/VuetifyLogo" */).then(c => c.default || c)
+export const LazyTopbar = import('../../components/topbar/Topbar.vue' /* webpackChunkName: "components/topbar/Topbar" */).then(c => c.default || c)
 export const LazySidebarFooter = import('../../components/sidebar/SidebarFooter.vue' /* webpackChunkName: "components/sidebar/SidebarFooter" */).then(c => c.default || c)
 export const LazySidebarList = import('../../components/sidebar/SidebarList.vue' /* webpackChunkName: "components/sidebar/SidebarList" */).then(c => c.default || c)
 export const LazySidebarListItem = import('../../components/sidebar/SidebarListItem.vue' /* webpackChunkName: "components/sidebar/SidebarListItem" */).then(c => c.default || c)
-export const LazyTopbar = import('../../components/topbar/Topbar.vue' /* webpackChunkName: "components/topbar/Topbar" */).then(c => c.default || c)
