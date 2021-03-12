f03e56c9b36f6e97edb556b35cfdd9375843c776
Federico Di Filitto <federico211difi@gmail.com>
2021-03-12 14:20:11 +0100

    Added remove button, resolved mutating prop warning

--- lib/templates/components/editing/MediaElement.vue
+++ lib/templates/components/editing/MediaElement.vue
@@ -1,89 +1,103 @@
 <template>
-    <v-row>
-        <v-col cols="12">
-            <v-card
+  <v-row>
+    <v-col cols="12">
+      <v-card
+        :dark="this.dark"
+        :light="!this.dark"
+      >
+        <v-img
+            v-if="child_media"
+            :src="child_media.file"
+            aspect-ratio="1"
+        > </v-img>
+
+        <v-btn v-else block text height="300" @click="editing = true">
+          <v-icon x-large>mdi-plus-box</v-icon>
+        </v-btn>
+
+        <media-manager-dialog
+          v-model="editing"
+          select="single"
+          v-on:selectionChange="update"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-on="on"
+              v-bind="attrs"
+              v-if="child_media"
+              absolute
+              fab
+              bottom
+              right
+              class="mb-10 mr-15"
             >
-                <v-img
-                    v-if="media"
-                    :src="media.file"
-                    aspect-ratio="1"
-                >
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </template>
+        </media-manager-dialog>
 
-                </v-img>
-
-
-                <v-btn
-                    v-else
-                    block
-                    text
-                    height=300
-                    @click="editing=true"
-                >
-                    <v-icon x-large>mdi-plus-box</v-icon>
-                </v-btn>
-
-
-
-
-                <media-manager-dialog
-                    v-model="editing"
-                    select="single"
-                    v-on:selectionChange="update"
-                >
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                            v-on="on"
-                            v-bind="attrs"
-
-                            v-if="media"
-                            absolute
-                            fab
-                            bottom
-                            right
-                            class="mb-10"
-                        >
-                            <v-icon>mdi-pencil</v-icon>
-                        </v-btn>
-                    </template>
-                </media-manager-dialog>
-            </v-card>
-        </v-col>
-        
-
-    </v-row>
+        <v-btn
+            v-if="child_media"
+            fab
+            absolute
+            bottom
+            right
+            class="mb-10"
+            @click="remove"
+        >
+            <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default{
-    data(){
-        return{
-            editing: false,
-        }
-    },
+export default {
+  data() {
+    return {
+      editing: false,
+      child_media: null,
+    };
+  },
 
-   model: {
-        prop: 'media',
-        event: 'changed-media'
-    },
+  model: {
+    prop: "media",
+    event: "changed-media",
+  },
 
-    mounted(){
-        console.log(this.media)
-    },
+  mounted(){
+    if(this.media)
+        this.child_media=this.media
+  },
 
-    props: {
-        media: {
-            type: Object,
-        },
+  props: {
+    media: {
+      type: Object,
+      default: () => null,
     },
-
-
-    methods: {
-        update(selection){
-            this.media=selection
-            this.$emit("changed-media", selection);
-        }
+    dark: {
+        type: Boolean,
+        default: () => false,
     },
-}
+  },
+
+  methods: {
+    update(selection) {
+      this.child_media=selection
+      this.$emit("changed-media", selection);
+    },
+    remove(){
+        this.child_media=null
+        this.$emit("changed-media", null);
+    }
+  },
+
+  watch: {
+      media(){
+          this.child_media=this.media
+      }
+  }
+
+};
 </script>
-
-
--- lib/templates/pages/settings.vue
+++ lib/templates/pages/settings.vue
@@ -6,8 +6,16 @@
       </v-col>
     </v-row>
 
+    <v-row class="my-4">
+      <v-btn
+        @click="changeFakeMedia"
+      >
+        CHANGE FAKE-MEDIA
+      </v-btn>
+    </v-row>
+
     <v-row>
-      <MediaElement/>
+      <MediaElement v-model="fakeMedia"/>
     </v-row>
   </v-container>
 </template>
@@ -21,13 +29,26 @@ export default {
     return {
       fakeMedia: {
         file: 'https://tcdn.storeden.com/gallery/5f60ba115fb8e0cbd23c8f82'
-      }
+      },
+      links: [
+        'https://i.picsum.photos/id/559/200/200.jpg?hmac=YMqBxDHO4-upCRoX_Ho2FNQg40ANP2MndFXD8sPsSAc',
+        'https://i.picsum.photos/id/822/200/200.jpg?hmac=pXgRn-rbZIan3GYBv9xCVsdyt_Kzq5Q_d0AbLnzeT3k',
+        'https://i.picsum.photos/id/337/200/300.jpg?hmac=0CnfGB9OuB4D8IneXqgjPMaGgLSHBKRjSkl_ITBmDxQ',
+        'https://i.picsum.photos/id/117/200/200.jpg?hmac=hAXY0lMbkjkxYIKxW0UjtazVquGY1NCu3ruHLJGc4gs',
+      ]
     };
+  },
+  methods:{
+    changeFakeMedia(){
+      const length = this.links.length;
+      const i = Math.floor(Math.random() * length);
+      this.fakeMedia={file: this.links[i]}
+    }
   },
   watch: {
     fakeMedia(){
       console.log(this.fakeMedia)
     }
-  }
+  },
 };
 </script>
