da52a4ff91dd0a5b6cbbd379db2587d7c2eb07c6
bnznamco <gabriele.baldi.01@gmail.com>
2021-03-31 11:20:57 +0200

    added confirmation dialog

---
+++ lib/templates/components/ConfirmDialog.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <slot v-bind="{ on, attrs }" name="activator"></slot>
+    <v-dialog v-model="dialog" width="800">
+      <v-card v-bind="$attrs">
+        <v-card-title class="headline">{{question}}</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="close">Cancel</v-btn>
+          <v-btn text @click="accept">OK</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      response: null,
+      dialog: false,
+      attrs: { ...this.$attrs },
+      on: {
+        click: (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          this.dialog = !this.dialog;
+        },
+      },
+    };
+  },
+  props: {
+    value: {
+      type: Boolean,
+    },
+    question: {
+      type: String,
+      default: "Are you sure of what you are doing?",
+    },
+  },
+  watch: {
+    value(val) {
+      this.dialog = val;
+    },
+    dialog(val) {
+      this.$emit("input", val);
+      this.$emit(val ? "open" : "close");
+      if (!val && this.reject) {
+        this.reject();
+      }
+    },
+  },
+  methods: {
+    open() {
+      this.dialog = true;
+      return new Promise((resolve, reject) => {
+        this.resolve = resolve;
+        this.reject = reject;
+      });
+    },
+    accept() {
+      this.response = true;
+      this.close();
+    },
+    close() {
+      if (this.resolve) {
+        this.resolve(this.response);
+      }
+      this.dialog = false;
+    },
+  },
+};
+</script>
