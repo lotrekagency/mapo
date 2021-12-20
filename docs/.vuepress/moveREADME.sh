474817fcb1aa984609381e12088f2d30a86b9926
bnznamco <gabriele.baldi.01@gmail.com>
2021-12-20 17:46:06 +0100

    docs: add changelogs to doc generation

--- docs/.vuepress/moveREADME.sh
+++ docs/.vuepress/moveREADME.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 source="./packages/@mapomodule/uikit"
 destination="./docs"
-
 destination=$(cd -- "$destination" && pwd)
-cd -- "$source" && find . -name "*.md" -exec sh -c '
+changelog_dir="$destination/Changelog/"
+
+mkdir -p $changelog_dir
+cp ./CHANGELOG.md $changelog_dir/README.md
+
+cd -- "$source" && find . -name "*.md" -not -name "CHANGELOG.md" -exec sh -c '
   mkdir -p "$0/${1%.*}/"
   mv "$1" "$0/${1%.*}/README.md"
 ' "$destination" {} \;
