1c45cb1dd97f72c4fbf5e607c40e31c1ee615efb
bnznamco <gabriele.baldi.01@gmail.com>
2022-03-12 15:51:04 +0100

    feat(theme): added dark/light mode switcher

--- packages/@mapomodule/uikit/components/layout/Topbar/Topbar.vue
+++ packages/@mapomodule/uikit/components/layout/Topbar/Topbar.vue
@@ -8,20 +8,24 @@
   >
     <v-app-bar-nav-icon @click.stop="drawerReverse()" />
     <v-toolbar-title v-text="title" />
+    <v-spacer />
+    <v-icon @click="toggleTheme">mdi-theme-light-dark</v-icon>
   </v-app-bar>
 </template>
 
 <script>
-
 export default {
   name: "Topbar",
   props: {
-    title: String
+    title: String,
   },
   methods: {
     drawerReverse() {
       this.$store.dispatch("mapo/app/toggleSidebarDrawer");
-    }
-  }
+    },
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
 };
 </script>
