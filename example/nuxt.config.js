2ad4598d99427717d58da33c29086a99e1086a47
bnznamco <gabriele.baldi.01@gmail.com>
2022-04-03 16:56:28 +0200

    chore: update dependencies

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   mapo: {
     integrations: {
       camomilla: {
-        location: "@mapomodule/integrations-camomilla-api",
+        location: "@mapomodule/mapo-integrations-camomilla",
         configuration: {
           api: {
             target: 'http://localhost:8000',
