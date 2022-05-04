72a1002144ff0be1c016a39f46f8ea712a950825
bnznamco <gabriele.baldi.01@gmail.com>
2022-05-04 11:33:25 +0200

    fix(utils): better File recognition for deepClone util

--- packages/@mapomodule/utils/helpers/objHelpers.js
+++ packages/@mapomodule/utils/helpers/objHelpers.js
@@ -1,7 +1,7 @@
 function deepClone(obj) {
     const clone = obj instanceof Array ? Object.assign([]) : Object.assign({});
     for (const i in obj) {
-        if (obj[i] != null && typeof (obj[i]) == 'object' && !(obj[i] instanceof File) && !(obj[i] instanceof Date)) {
+        if (obj[i] != null && typeof (obj[i]) == 'object' && !(isFileOrBlob(obj[i])) && !(obj[i] instanceof Date)) {
             clone[i] = deepClone(obj[i]);
         } else {
             clone[i] = obj[i];
