575c2853eb774ca4c635ce099302a429cc479be5
mino89 <pierdomenicoreitano@gmail.com>
2022-08-04 13:48:58 +0200

    feat: implemented media preview through MediaPreview component

--- packages/@mapomodule/uikit/components/Form/fields/MediaField.vue
+++ packages/@mapomodule/uikit/components/Form/fields/MediaField.vue
@@ -12,20 +12,21 @@
       <div style="position: relative">
         <v-img
           v-if="mediaExists"
-          :src="internalValue.is_image && internalValue.file || null"
-          :lazy-src="internalValue.is_image && internalValue.thumbnail || null"
-          :class="{'grey lighten-2': !internalValue.is_image}"
           v-bind="{
-            aspectRatio,
-            contain,
             height,
             width,
             maxWidth,
             maxHeight,
             minWidth,
             minHeight,
+            ...$attrs,
           }"
         >
+          <MediaPreview 
+          :key="componentKey"
+          v-if="mediaExists" 
+          :media="internalValue" 
+          />
           <template v-slot:placeholder>
             <v-row
               v-if="internalValue"
@@ -38,10 +39,24 @@
                 indeterminate
                 color="grey"
               ></v-progress-circular>
-              <div class="d-flex flex-column align-center justify-space-between fill-height" style="width: 100%" v-else>
-              <div></div>
-              <v-icon size="70px" color="grey">mdi-file</v-icon>
-              <span class="grey--text text--darken-3 text-truncate pl-1 pr-8" style="width: 100%">{{fileName}}</span>
+              <div
+                class="
+                  d-flex
+                  flex-column
+                  align-center
+                  justify-space-between
+                  fill-height
+                "
+                style="width: 100%"
+                v-else
+              >
+                <div></div>
+                <v-icon size="70px" color="grey">mdi-file</v-icon>
+                <span
+                  class="grey--text text--darken-3 text-truncate pl-1 pr-8"
+                  style="width: 100%"
+                  >{{ fileName }}</span
+                >
               </div>
             </v-row>
           </template>
@@ -64,7 +79,13 @@
           <v-card-title>{{ label }}</v-card-title>
 
           <v-card-actions>
-            <v-btn tile :disabled="readonly" @click="editing = true" block :min-height="minHeight">
+            <v-btn
+              tile
+              :disabled="readonly"
+              @click="editing = true"
+              block
+              :min-height="minHeight"
+            >
               <v-icon size="80"> mdi-plus-circle-outline </v-icon>
             </v-btn>
           </v-card-actions>
@@ -79,13 +100,23 @@
           </v-row>
           <v-row>
             <v-col cols="6">
-              <v-btn :disabled="readonly" @click="editing = true" fab v-bind="$attrs">
+              <v-btn
+                :disabled="readonly"
+                @click="editing = true"
+                fab
+                v-bind="$attrs"
+              >
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
             </v-col>
 
             <v-col cols="6">
-              <v-btn :disabled="readonly" @click="confirmDelete" fab v-bind="$attrs">
+              <v-btn
+                :disabled="readonly"
+                @click="confirmDelete"
+                fab
+                v-bind="$attrs"
+              >
                 <v-icon>mdi-delete</v-icon>
               </v-btn>
             </v-col>
@@ -100,14 +131,9 @@
           v-bind="$attrs"
         >
         </media-manager-dialog>
-
       </div>
     </v-hover>
-    <v-messages
-      v-model="errorMessages"
-      color="error"
-      class="mt-2"
-    />
+    <v-messages v-model="errorMessages" color="error" class="mt-2" />
   </div>
 </template>
 
@@ -122,6 +148,7 @@ export default {
       internalValue: null,
       editing: false,
       isHovered: false,
+      componentKey: 0,
     };
   },
 
@@ -134,7 +161,7 @@ export default {
     },
     errorMessages: {
       type: String | Array,
-      default: () => []
+      default: () => [],
     },
     rmAddBtn: {
       type: Boolean,
@@ -188,22 +215,33 @@ export default {
       return this.isHovered && this.mediaExists;
     },
     fileName() {
-      return this.internalValue && this.internalValue.file && this.internalValue.file.split("/").pop();
+      return (
+        this.internalValue &&
+        this.internalValue.file &&
+        this.internalValue.file.split("/").pop()
+      );
     },
   },
 
   methods: {
     update(val) {
       this.internalValue = val;
+      this.forceRerender()
     },
     confirmDelete() {
       this.$mapo.$confirm
         .open({
           title: this.$t("mapo.remove"),
           question: this.$t("mapo.mediaField.confirmRemove"),
-          approveButton: { text: this.$t("mapo.remove"), attrs: { color: "red", text: true } }
+          approveButton: {
+            text: this.$t("mapo.remove"),
+            attrs: { color: "red", text: true },
+          },
         })
         .then((res) => (res ? (this.internalValue = null) : null));
+    },
+    forceRerender() {
+      this.componentKey += 1;  
     },
     isImage(val) {
       if (!val?.is_image && val?.mime_type.includes("image")) {
@@ -219,12 +257,12 @@ export default {
       }
     },
     internalValue(val) {
-      this.isImage(val)
+      this.isImage(val);
       this.$emit("input", val);
     },
   },
-  mounted(){
-    this.internalValue = this.val
-  }
+  mounted() {
+    this.internalValue = this.value;
+  },
 };
 </script>
