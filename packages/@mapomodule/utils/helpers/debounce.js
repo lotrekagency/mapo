3c9a02509ffd8277d362c30eb2cfafabdde995b7
Edoardo Grassi <edo.g@hotmail.it>
2022-09-12 20:02:58 +0200

    feat: added memoized debounce

--- packages/@mapomodule/utils/helpers/debounce.js
+++ packages/@mapomodule/utils/helpers/debounce.js
@@ -1,7 +1,8 @@
 function debounce(func, wait, immediate) {
     var timeout;
     return function () {
-        var context = this, args = arguments;
+        var context = this,
+            args = arguments;
         var later = function () {
             timeout = null;
             if (!immediate) func.apply(context, args);
@@ -11,8 +12,31 @@ function debounce(func, wait, immediate) {
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
     };
-};
+}
+
+function memoizedDebounce(
+    func,
+    wait,
+    immediate,
+    argsSerializer = (args) => JSON.stringify(args)
+) {
+    var timeout = {};
+    return function () {
+        var context = this,
+            args = arguments,
+            serializedArgs = argsSerializer(arguments);
+        var later = function () {
+            timeout[serializedArgs] = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout[serializedArgs];
+        clearTimeout(timeout[serializedArgs]);
+        timeout[serializedArgs] = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
 
 module.exports = {
-    debounce
-}
+    debounce,
+    memoizedDebounce,
+};
