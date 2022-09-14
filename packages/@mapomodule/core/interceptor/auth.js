59b2f5cb3dfd0becd78df02f30bc32967643e276
bnznamco <gabriele.baldi.01@gmail.com>
2022-09-14 11:19:22 +0200

    fix(interceptor): remove token injection for axios requests

--- packages/@mapomodule/core/interceptor/auth.js
+++ packages/@mapomodule/core/interceptor/auth.js
@@ -1,13 +1,5 @@
 module.exports = ({ $axios, store }) => {
     if ($axios && store) {
-        $axios.onRequest((config) => {
-            const token = store.getters['mapo/user/token']
-            if (token && token !== 'undefined') {
-                config.headers['Authorization'] = `Token ${token}`
-            }
-            return config
-        })
-
         $axios.onResponse((response) => {
             if (response.status === 401) {
                 store.dispatch('mapo/user/logout');
