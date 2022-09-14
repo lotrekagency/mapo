1d49cd72e141888ef7aae91ac0ddb13f8d0fce7a
bnznamco <gabriele.baldi.01@gmail.com>
2022-09-14 11:18:51 +0200

    fix(middleware): fix auth middleware to correclty redirect based on router base

--- packages/@mapomodule/core/middleware/auth.js
+++ packages/@mapomodule/core/middleware/auth.js
@@ -1,9 +1,10 @@
 import Middleware from '../../middleware' // little hack to inject middleware in root project :P
 
-Middleware.auth = Middleware.auth || function ({ route, redirect, query, store }) {
+Middleware.auth = Middleware.auth || function ({ route, redirect, query, store, app }) {
+    const { router: { options } } = app;
     const isLoggedIn = store.getters['mapo/user/isLoggedIn']
     if (!isLoggedIn) {
         query['redirect'] = route.path
-        return redirect('/login', query)
+        return redirect(`${options.base || '/'}login`, query)
     }
 }
