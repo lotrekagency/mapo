72c0db8d71b89bc427ac7f2bd0816970abfd085a
bnznamco <gabriele.baldi.01@gmail.com>
2022-11-09 17:17:01 +0100

    chore: update media example

--- example/pages/mediamanager-example.vue
+++ example/pages/mediamanager-example.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="d-flex flex-column fill-height">
-    <MediaManager class="transparent" />
-    <v-spacer></v-spacer>
-    <div class="d-flex justify-center">
-      <media-manager-dialog v-model="dialog" select="multi">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="red lighten-2 mb-4" v-bind="attrs" v-on="on">
-            {{ $t("openDialog") }}
-          </v-btn>
-        </template>
-      </media-manager-dialog>
-    </div>
+    <MediaManager />
+    <media-manager-dialog v-model="dialog" select="multi">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red"
+          v-bind="attrs"
+          v-on="on"
+          fixed
+          bottom
+          right
+          fab
+          icon
+          dark
+          small
+        >
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </template>
+    </media-manager-dialog>
   </div>
 </template>
-
-<i18n lang="yaml">
-en:
-  openDialog: "Open media dialog"
-it:
-  openDialog: "Apri modale media"
-</i18n>
 
 <script>
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
     };
-  }
+  },
 };
 </script>
