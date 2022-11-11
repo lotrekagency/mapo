0ef6361bdf8547a9423fee02fa87ecbf3e5b6512
bnznamco <gabriele.baldi.01@gmail.com>
2022-11-11 14:29:40 +0100

    chore: fix doc generationg

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,9 +5,15 @@ import injectMapo from '../../packages/@mapomodule/core/index'
 import vuetify from '../../packages/@mapomodule/uikit/defaults/vuetify'
 import VueI18n from 'vue-i18n'
 import enUsTrans from "../../packages/@mapomodule/uikit/translations/en-US"
+import Vuex from 'vuex'
+import injectStore from '../../packages/@mapomodule/store/index'
 
 
 export default ({ Vue, options, router, siteData }) => {
+    Vue.use(Vuex)
+    const store = new Vuex.Store()
+    injectStore({ store })      
+    Vue.mixin({ store })
     function fakeHttp() { return Promise.reject({ detail: "This is a demo environment.. No backend connection.. " }) }
     const fakeAxios = { "$get": fakeHttp, "$post": fakeHttp, "$patch": fakeHttp, "$delete": fakeHttp }
     injectMapo({ $axios: fakeAxios }, (_, plugin) => Vue.prototype.$mapo = plugin)
