7b15fcc846b829555d7a0aa7b1f309f57107fd66
bnznamco <gabriele.baldi.01@gmail.com>
2022-11-14 10:16:54 +0100

    fix: fix index layout

--- packages/@mapomodule/uikit/pages/index.vue
+++ packages/@mapomodule/uikit/pages/index.vue
@@ -1,21 +1,23 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="12">
-      <div class="mapo-info">
-        <h1 id="mapo">
-          <code>mapomodule {{ version }}</code>
-        </h1>
-        <br>
-        <br>
-        <code>{{ $t('mapo.index.welcomeMessage') }}</code>
-        <br>
-        <br>
-        <code>
-          ## TODO INSERT HERE A LITTLE TUTORIAL.
-        </code>
-      </div>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="12">
+        <div class="mapo-info">
+          <h1 id="mapo">
+            <code>mapomodule {{ version }}</code>
+          </h1>
+          <br>
+          <br>
+          <code>{{ $t('mapo.index.welcomeMessage') }}</code>
+          <br>
+          <br>
+          <code>
+            ## TODO INSERT HERE A LITTLE TUTORIAL.
+          </code>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
