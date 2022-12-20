48e4c7b047a1062464d823ddeebdc60b96ea11a8
bnznamco <gabriele.baldi.01@gmail.com>
2022-12-20 14:06:16 +0100

    feat: style scrollbars

--- packages/@mapomodule/uikit/layouts/default.vue
+++ packages/@mapomodule/uikit/layouts/default.vue
@@ -22,13 +22,26 @@ html { overflow-y: auto }
     }
   }
 }
+::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+}
+::-webkit-scrollbar-track {
+    background-color: var(--v-scrollbar-track-color);
+}
+::-webkit-scrollbar-thumb {
+  background-color: var(--v-primary-base);
+}
 </style>
 
 <script>
 import initDraggable from "@mapomodule/utils/plugins/v-draggable-modals";
+import { setScrollbarTrackColor } from "@mapomodule/utils/helpers/cssVariables"
 export default {
   mounted() {
     initDraggable();
+    setScrollbarTrackColor(this.$vuetify.theme.dark)
   },
+  watch: { "$vuetify.theme.dark": setScrollbarTrackColor }
 };
 </script>
