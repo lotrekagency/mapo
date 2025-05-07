262faa1e21ad09f29713eceee38d453a64f7cabf
Michele Abbrescia <michele.abbrescia@lotrek.it>
2025-05-07 11:15:47 +0200

    fix: update integration camomilla in example and comment nx target to avoid loop

--- apps/example/nuxt.config.ts
+++ apps/example/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils'],
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@mapomodule/integrations-camomilla'],
   extends: ['mapomodule'],
   vuetify: {
     vuetifyOptions: {
--- packages/mapomodule/nuxt.config.ts
+++ packages/mapomodule/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils', '@nuxt/eslint', '@mapomodule/integrations-camomilla'],
+  modules: ['@nuxt/test-utils', '@nuxt/eslint'],
   extends: [
     '@mapomodule/uikit'
   ],
